835ff115e7eb92f45d8f5dc84c8ab6e03112b3b2
KramWell <43101805+kramwell@users.noreply.github.com>
2022-01-21 13:18:45 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jah1.com
  23/SEPT/2021 - Simple yet effective website produced for irish artist Jah1
+
+Live site > https://jah1.com
---
+++ source/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Official Site for Irish Artist Jah1 [JAH1.COM]</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<style>
+
+		body {
+			background: url(1.jpg) no-repeat center;
+			background-size: cover;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			-ms-background-size: cover;
+			background-attachment: fixed;
+			font-family: 'PT Mono', monospace;
+		}
+
+		@media only screen and (min-width: 769px) {
+		
+			img.toplogo {
+			padding-top: 50px;
+			width: 60%;
+			}		
+
+			img.enterlogo {
+			padding-bottom: 100px;
+			width: 20%;
+			}	
+		
+		}
+
+		@media only screen and (max-width: 768px) {
+		
+			img.toplogo {
+			padding-top: 50px;
+			width: 80%;
+			}		
+
+			img.enterlogo {
+			padding-bottom: 100px;
+			width: 40%;
+			}	
+		
+		}
+
+		div.footer {
+		  background: rgba(255, 255, 255, 0.2)
+		}
+
+	</style>
+
+</head>
+<body>
+
+<div class="container">
+  <img src="lce.png" class="mx-auto d-block toplogo"> 
+</div>
+
+<div class="container fixed-bottom">
+  <a href="https://linktr.ee/jah1official"><img src="enter.png" class="mx-auto d-block img-fluid enterlogo"><a>
+</div>
+
+<div class="footer fixed-bottom p-1">
+
+	<div class="container">
+	  <div class="row">
+		<div class="col-3">
+		  <a href="https://instagram.com/jah1official" target="_blank"><img class="mx-auto d-block" src=".\instagram.png" alt="instagram"><a>
+		</div>	
+		<div class="col-3">
+		   <a href="https://open.spotify.com/artist/3X5QJOSkMuR6wdlli6PR3X?si=I9VLbzcURu6cYHbG8w6Bbw" target="_blank"><img class="mx-auto d-block" src=".\spotify.png" alt="spotify"><a>
+		</div>
+		<div class="col-3">
+		  <a href="https://www.youtube.com/channel/UCmSjnE54eWft5DM2AEoskIQ?view_as=subscriber" target="_blank"><img class="mx-auto d-block" src=".\youtube.png" alt="youtube"><a> 
+		</div>
+		<div class="col-3">
+		   <a href="mailto:management@jah1.com?subject=Enquiry"><img class="mx-auto d-block" src=".\email.png" alt="email"><a>
+		</div>		
+	  </div>
+	</div>
+
+
+</div>
+
+</body>
+</html>
